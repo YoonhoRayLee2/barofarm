0b3a55140f7542e1809db52e4529a0048086ba30
YoonhoRayLee <lyho0307@dongguk.edu>
2026-05-22 18:23:43 +0900

    feat/app: FIX ecosyste.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,8 +2,9 @@ module.exports = {
   apps: [{
     name: 'barofarm',
     script: './src/index.ts',
-    // 여기에 복사한 경로를 그대로 넣으세요
     interpreter: '/home/ec2-user/.bun/bin/bun',
-    exec_mode: 'fork'
+    exec_mode: 'fork',
+    cwd: '/home/ec2-user/barofarm/server',
+    env_file: '/home/ec2-user/barofarm/server/.env'
   }]
 }
